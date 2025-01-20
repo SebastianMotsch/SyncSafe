eab869ad3196d491337f36ee9eb9aa00a3a3609c
Judson Lin <jl4656@drexel.edu>
2025-01-19 22:53:09 -0500

    Partial React Integration + src Images

--- SyncSafe/src/userInterface.jsx
+++ SyncSafe/src/userInterface.jsx
@@ -1,34 +1,44 @@
+function Header() {
+    return (
+    <header>
+        <div className="logo-container">
+            <img src="src/images/syncsafe_logo.png" alt="SyncSafe Logo" className="logo" />
+        </div>
+    </header>
+    )
+}
+
 function UrlLink() {
     return (
-    <div className="container"> <ul>
-        <li><a href="maininfo.html">INFO<div className="nav-icon"><img src="images/info_icon.png" alt="Info Icon" /></div></a></li>
-        <li><a href="scanner.html">SCANNER<div className="nav-icon"><img src="images/scanner_icon.png" alt="Scanner Icon" /></div></a></li>
-        <li><a href="faq.html">FAQs<div className="nav-icon"><img src="images/faq_icon.png" alt="FAQ Icon" /></div></a></li>
-        <li><a href="#">SERVICES<div className="nav-icon"><img src="images/services_icon.png" alt="Services Icon" /></div></a></li>
-        <li><a href="#">CONTACT<div className="nav-icon"><img src="images/contact_icon.png" alt="Contact Icon" /></div></a></li>
-    </ul> </div>
+    <ul>
+        <li><a href="maininfo.html">INFO<div className="nav-icon"><img src="src/images/info_icon.png" alt="Info Icon" /></div></a></li>
+        <li><a href="scanner.html">SCANNER<div className="nav-icon"><img src="src/images/scanner_icon.png" alt="Scanner Icon" /></div></a></li>
+        <li><a href="faq.html">FAQs<div className="nav-icon"><img src="src/images/faq_icon.png" alt="FAQ Icon" /></div></a></li>
+        <li><a href="#">SERVICES<div className="nav-icon"><img src="src/images/services_icon.png" alt="Services Icon" /></div></a></li>
+        <li><a href="#">CONTACT<div className="nav-icon"><img src="src/images/contact_icon.png" alt="Contact Icon" /></div></a></li>
+    </ul>
     )
 }
 
 function Footer() {
     return (
-    <>
+    <footer>
     <p> &copy; CI 102 - Team 6 - IoT Device Vulnerability Scanner. All rights reserved.</p>
-    </>
+    </footer>
     )
 }
 
 function PageSetup() {
     return (
     <>
+    <Header />
 
     <nav>
         <UrlLink />
     </nav>
 
-    <footer>
-        <Footer />
-    </footer>
+    
+    <Footer />
     </>
     )
 }
